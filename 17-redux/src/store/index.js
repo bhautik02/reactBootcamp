79b59f2e4a2f7f42461dff2846ba414a1bd614af
bhautik <bhautikjani1102@gmail.com>
2023-05-04 18:04:15 +0530

    may 4 redux basic complete

--- 17-redux/src/store/index.js
+++ 17-redux/src/store/index.js
@@ -1,51 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-// import { createStore } from "redux";
-import { createSlice } from "@reduxjs/toolkit";
-
-const counterInitialState = { counter: 0, showModal: true };
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: counterInitialState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggle(state) {
-      state.showModal = !state.showModal;
-    },
-  },
-});
-const authInitialState = { isAuthenticated: false };
-
-const authSlice = createSlice({
-  name: "Auth",
-  initialState: authInitialState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
+import counterReducer from "./counter";
+import authReducer from "./auth";
 
 const store = configureStore({
-  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+  reducer: { counter: counterReducer, auth: authReducer },
 });
 
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 export default store;
 
+// import { createStore } from "redux";
 // const counterReducer = (state = initialState, action) => {
 //   if (action.type === "INCREMENT") {
 //     return {
